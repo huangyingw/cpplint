5db28cf0d5d079f5c41e587ac3198e4bf7d7d368
huangyingw@linux_programming <huangyingw@gmail.com>
2010-11-01 17:06:14 +0800

    n

--- run.py
+++ run.py
@@ -1,1 +1,10 @@
 print "Hello, world"
+import fnmatch
+import os
+
+rootPath = './'
+pattern = '*.py' # Can include any UNIX shell-style wildcards
+
+for root, dirs, files in os.walk(rootPath):
+  for filename in fnmatch.filter(files, pattern):
+    print os.path.join(root, filename)
