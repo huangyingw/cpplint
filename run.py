2f0dd696c878772b4db4fe8d4200c14127cd99b8
huangyingw@linux_programming <huangyingw@gmail.com>
2010-11-01 18:23:43 +0800

    n

--- run.py
+++ run.py
@@ -1,9 +1,14 @@
 print "Hello, world"
 import fnmatch
+import scriptutil as SU
 import os
+import re
+
+flist = SU.ffind('.', shellglobs=('*.py', '*.hpp'))
+SU.printr(flist)
 
 rootPath = './'
-pattern = '*.(py|hpp)' # Can include any UNIX shell-style wildcards
+pattern = '*.py|*.hpp' 
 
 for root, dirs, files in os.walk(rootPath):
   for filename in fnmatch.filter(files, pattern):
