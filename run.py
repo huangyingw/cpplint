69ab8bb64d479371e54b3900b9ec6aa382d92af4
huangyingw@linux_programming <huangyingw@gmail.com>
2010-11-01 17:51:49 +0800

    n

--- run.py
+++ run.py
@@ -3,8 +3,10 @@ import fnmatch
 import os
 
 rootPath = './'
-pattern = '*.py' # Can include any UNIX shell-style wildcards
+pattern = '*.(py|hpp)' # Can include any UNIX shell-style wildcards
 
 for root, dirs, files in os.walk(rootPath):
   for filename in fnmatch.filter(files, pattern):
-    print os.path.join(root, filename)
+    '''print os.path.join(root, filename)'''
+    new_name=os.path.join(root, filename)
+    print new_name
