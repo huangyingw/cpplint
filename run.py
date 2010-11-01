7464fbcead9cd4521e8aebbda611cd3f47cc8fc0
huangyingw@linux_programming <huangyingw@gmail.com>
2010-11-01 18:39:57 +0800

    n

--- run.py
+++ run.py
@@ -7,11 +7,12 @@ import re
 flist = SU.ffind('.', shellglobs=('*.py', '*.hpp'))
 SU.printr(flist)
 
+'''
 rootPath = './'
-pattern = '*.py|*.hpp' 
+pattern = '*.hpp' 
 
 for root, dirs, files in os.walk(rootPath):
   for filename in fnmatch.filter(files, pattern):
-    '''print os.path.join(root, filename)'''
     new_name=os.path.join(root, filename)
     print new_name
+'''
