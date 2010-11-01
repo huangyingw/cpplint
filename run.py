c5ba6d52ec6e28e024bcc5ad5fbd27f66151de81
huangyingw@linux_programming <huangyingw@gmail.com>
2010-11-01 17:03:47 +0800

    n

--- run.py
+++ run.py
@@ -1,10 +1,4 @@
-print "Hello, world"
 import os
 
-def dirEntries(dir_name, subdir, *args):
-  fileList = []
-  if os.path.isfile(dirfile):
-  elif os.path.isdir(dirfile) and subdir:
-       print "Accessing directory:", dirfile
-  fileList.extend(dirEntries(dirfile, subdir, *args))
-  return fileList
+if os.path.isfile(dirfile):
+    print "Accessing directory:", dirfile
