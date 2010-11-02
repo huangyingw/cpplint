df79ba450b28234addcd0f6fabd21a0ae6447125
huangyingw@linux_programming <huangyingw@gmail.com>
2010-11-02 13:43:39 +0800

    n

--- cpplint.py
+++ cpplint.py
@@ -1008,9 +1008,9 @@ def FindAuthor(filename, lines, error):
     for line in xrange(1, min(len(lines), 110)):
       if re.search(r'Author', lines[line], re.I): break
     else:                       # means no copyright line was found
-      error(filename, 0, 'legal/copyright', 5,
-            'No copyright message found.  '
-            'You should have a line: "Copyright [year] <Copyright Owner>"')
+      error(filename, 0, 'legal/author', 5,
+            'No author message found.  '
+            'You should have a line: "Author: <email>"')
 
 def CheckForCopyright(filename, lines, error):
   """Logs an error if no Copyright message appears at the top of the file."""
